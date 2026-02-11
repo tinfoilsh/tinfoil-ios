c8b667fb6121d2a91d2dce52275429cca253d5ba
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-02-10 23:21:37 -0500

    fix: chat id generation

--- TinfoilChat/Config/Constants.swift
+++ TinfoilChat/Config/Constants.swift
@@ -65,6 +65,7 @@ enum Constants {
         static let maxReverseTimestamp: Int = 9999999999999
         static let reverseTimestampDigits: Int = String(maxReverseTimestamp).count
 
+        static let createdAtFallbackThresholdSeconds: TimeInterval = 5.0
         static let uploadBaseDelaySeconds: TimeInterval = 1.0
         static let uploadMaxDelaySeconds: TimeInterval = 8.0
         static let uploadMaxRetries: Int = 3
