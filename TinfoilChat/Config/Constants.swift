5f96bc0fc5000c3868bd87670ba0fca0311b9266
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-02-13 14:52:40 -0500

    feat: persist cloud vs local

--- TinfoilChat/Config/Constants.swift
+++ TinfoilChat/Config/Constants.swift
@@ -59,6 +59,7 @@ enum Constants {
 
     enum CloudSync {
         static let enabledKey = "cloudSyncEnabled"
+        static let activeTabKey = "cloudSyncActiveTab"
         static let clipboardExpirationSeconds: TimeInterval = 300
     }
 
