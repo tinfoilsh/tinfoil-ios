0b9d3a7eb331da0282501375d730cf9070bcc93d
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-09-19 10:50:10 -0700

    fix: prevent the iPad interface from staying blurred

--- TinfoilChat/Views/ChatView.swift
+++ TinfoilChat/Views/ChatView.swift
@@ -73,8 +73,6 @@ struct ChatContainer: View {
         NavigationView {
             mainContent
                 .background(Color.chatBackground(isDarkMode: colorScheme == .dark))
-                .blur(radius: isAnySheetPresented && UIDevice.current.userInterfaceIdiom == .pad ? 10 : 0)
-                .animation(.easeInOut(duration: 0.2), value: isAnySheetPresented)
         }
         .onChange(of: isAnySheetPresented) { _, presented in
             if presented && isSidebarOpen {
