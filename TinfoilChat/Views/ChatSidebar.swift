eee9217327f375fd9185959353e168e8d2aa0d57
Daniel MS <danielmccannsayles@gmail.com>
2026-09-22 14:15:35 -0700

    Remove icons, these are subsumed by tap & swipe

--- TinfoilChat/Views/ChatSidebar.swift
+++ TinfoilChat/Views/ChatSidebar.swift
@@ -1242,20 +1242,6 @@ struct ChatListItem: View {
                             
                             Spacer()
                         }
-                        
-                        if isSelected && showEditDelete && !chat.isBlankChat {
-                            // Edit and Delete buttons (not shown for new/blank chats)
-                            HStack(spacing: 12) {
-                                Button(action: onEdit) {
-                                    Image(systemName: "square.and.pencil")
-                                        .foregroundColor(.gray)
-                                }
-                                Button(action: onDelete) {
-                                    Image(systemName: "trash")
-                                        .foregroundColor(.gray)
-                                }
-                            }
-                        }
                     }
                 }
                 
